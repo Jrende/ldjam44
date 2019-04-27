13eca54f943bc5eb1779b1e17250a055169a8075
Johan Rende <johan@rende.se>
2019-04-27 13:49:22 +0200

    Mer stuffs

---
+++ Assets/HandMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HandMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float distance = 50f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            agent.destination = hit.point;
+        }
+    }
+}
---
+++ Assets/Scripts/BoidMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoidMovement : MonoBehaviour
+{
+    public GameObject palm;
+    private NavMeshAgent agent;
+    public Transform target;
+
+    void Start()
+    {
+        agent = GetComponentInChildren<NavMeshAgent>();
+    }
+
+    
+    void Update() {
+        agent.destination = target.position;
+    }
+}
