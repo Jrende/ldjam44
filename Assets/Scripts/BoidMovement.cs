ea463c4a7cdde464c8b16478b26c064fb496dc31
Johan Rende <johan@rende.se>
2019-04-27 19:51:44 +0200

    💰💰💰

--- Assets/Scripts/BoidMovement.cs
+++ Assets/Scripts/BoidMovement.cs
@@ -8,12 +8,22 @@ public class BoidMovement : MonoBehaviour
     private NavMeshAgent agent;
     private Vector3 target;
     GameObject[] hands;
-    float avoidDist = 2f;
+    float avoidDist = 4f;
+    float calmDist = 5f;
+    float panicSpeed = 10.0f;
+    float wanderSpeed = 1.5f;
+    Random rnd = new Random();
+
+    enum State { Panic, Wander };
+    State currentState = State.Wander;
 
     void Start()
     {
         hands = GameObject.FindGameObjectsWithTag("hand");
         agent = GetComponentInChildren<NavMeshAgent>();
+
+        target = getRandomPosition();
+        
         //agent.destination = target;
     }
 
@@ -23,16 +33,46 @@ public class BoidMovement : MonoBehaviour
         //agent.destination = target.position;
         Vector3 closestHand = getClosestHandPosition();
         //Debug.Log("Closest hand pos: " + closestHand);
-        float dist = Vector3.Distance(transform.position, closestHand);
-        Debug.Log("dist:" + dist);
-        if (dist < avoidDist)
+        float handDist = Vector3.Distance(transform.position, closestHand);
+        if (handDist < avoidDist)
         {
             Debug.Log("Avoid the hand!!");
-            target = (transform.position - closestHand).normalized * avoidDist;
-            Debug.Log("Go to " + target);
+            target = transform.position + (transform.position - closestHand).normalized * calmDist;
             Debug.DrawLine(target, target + Vector3.up);
             agent.destination = target;
+            agent.speed = panicSpeed;
+            agent.acceleration = 15f;
+            currentState = State.Panic;
         }
+        if (handDist > calmDist && currentState == State.Panic)
+        {
+            Debug.Log("All's chill again");
+            currentState = State.Wander;
+            agent.speed = wanderSpeed;
+            agent.acceleration = 8f;
+        }
+        if (currentState == State.Wander)
+        {
+            
+            float distToTarget = Vector3.Distance(transform.position, target);
+            Debug.Log("target " + target);
+            Debug.Log("dist " + distToTarget);
+            if (distToTarget < 2.0f)
+            {
+                target = getRandomPosition();
+                Debug.Log("Arrive! Got to" + target);
+            }
+            agent.destination = target;
+        }
+        Debug.DrawLine(transform.transform.position, target);
+    }
+
+    Vector3 getRandomPosition()
+    {
+        return transform.position + new Vector3(
+            Random.Range(-1.0f, 1.0f),
+            0,
+            Random.Range(-1.0f, 1.0f)).normalized * Random.Range(2.0f, 6.0f);
     }
 
     Vector3 getClosestHandPosition()
