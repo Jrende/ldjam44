a6c8210dc71021f0ee85bda4cac985a24e608a11
Johan Rende <johan@rende.se>
2019-04-27 14:31:11 +0200

    Added mynt

---
+++ Assets/Scripts/AxisHandMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AxisHandMovement : MonoBehaviour
+{
+    public int index;
+    //private Transform transform;
+
+    void Start()
+    {
+        //agent = GetComponent<NavMeshAgent>();
+        //rigidBody = GetComponent<Rigidbody>();
+        //transform = GetComponent<Transform>();            
+    }
+
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal" + index);
+        float vert = Input.GetAxis("Vertical" + index);
+
+        transform.position += new Vector3(horiz, 0, vert);
+    }
+}
--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -5,10 +5,14 @@ public class HandMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     public float distance = 50f;
-
+    private Rigidbody rigidBody;
+    //private Transform transform;
+        
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
+        //rigidBody = GetComponent<Rigidbody>();
+        //transform = GetComponent<Transform>();            
     }
 
     void Update()
@@ -17,7 +21,11 @@ public class HandMovement : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, distance))
         {
-            agent.destination = hit.point;
+            Vector3 v = hit.point;
+            v.y = 0;
+            //rigidBody.position = v;
+            transform.position = v;
+
         }
     }
 }
