5e48e1ad0675a7d19c63d3542bc960af64c4447d
Johan Rende <johan@rende.se>
2019-04-28 15:35:14 +0200

    Added sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource ratSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var states = getStates();
+        int totalCoins = states.Count;
+        int numberOfPanicked = states.Where(state => state == BoidMovement.State.Panic).Count();
+        float volume = ((float) numberOfPanicked / (float) totalCoins);
+        Debug.Log("volume: " + volume);
+        ratSound.volume = volume;
+    }
+
+    List<BoidMovement.State> getStates()
+    {
+        List<GameObject> coinList = GameObject.FindGameObjectsWithTag("$").ToList();
+        return coinList.Select(coin => coin.GetComponent<BoidMovement>().GetCurrentState()).ToList();
+    }
+
+}
