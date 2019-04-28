19d9c55de857168798da06ebca55cfe52a28b97e
Ooe <ola.rende@gmail.com>
2019-04-28 17:19:44 +0200

    Add more angry comments

--- Assets/Scripts/AngryCommentGenerator.cs
+++ Assets/Scripts/AngryCommentGenerator.cs
@@ -11,14 +11,23 @@ public class AngryCommentGenerator : MonoBehaviour {
         new string[]{
             "Can you hurry up?", "What's taking so long?", "Hey man, I'm in a rush here.", "Dude, like, seriously?",
             "Is this gonna take all day?", "Buddy, there's a lotta folks queueing.", "Hot damn, this is like the DMV all over!",
-            "Are you buying up the whole store or what?", "Are you paying or are you not?"
+            "Are you buying up the whole store or what?", "Are you paying or are you not?",
+            "Are you waiting for the federal bank to pay for you or what?",
+            "Dude, pay or that can will get neatly packaged up yours!",
+            "Dude, pay or I’m calling the police, they’re gonna make you pay in lead, bro…"
+
         }, new string[]{
             "Comrade, hurry it up.", "I feel like been queueing since Perestroika!", "Blyad, can you be any slower?",
-            "This is like queueing for buying Lada.", "Are you trying to bring down system by being slow, comrade?"
+            "This is like queueing for buying Lada.", "Are you trying to bring down system by being slow, comrade?",
+            "Comrade, are you waiting for inflation to make your Vodka cheaper??",
+            "Drog, the vodka will evaporate, pay up and drink it fast, for the motherland!",
+            "Are we waiting for the next meltdown? Pay up already comrade!",
+            "I’m turning into Babuchka, pay, pay, plati!!!!"
         }, new string[]{
             "Dost thou have naught but idle time?", "Make haste, knave!", "Hath thou not a care in the world!?",
             "Must the sun set beneath before thine affairs are in order?", "The course of affairs is not running smooth this day...",
-            "A cashier! A cashier! My kingdom for a cashier!", "It doth feel like I've waited a forthnight"
+            "A cashier! A cashier! My kingdom for a cashier!", "It doth feel like I've waited a forthnight",
+            "Dost thou wish to pay with thine blood, peon!?"
         }
     };
     private TextMeshProUGUI[] uiTexts;
